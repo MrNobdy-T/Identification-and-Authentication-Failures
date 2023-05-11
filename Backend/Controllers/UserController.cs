c6f7cd0b8a2dcf20c13ad9e25023e19184d36abc
Soma Oláh <107466825+olahsoma@users.noreply.github.com>
2023-05-11 12:32:23 +0200

    init backend

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
+
+            return View(context.GetUsers());
+        }
+    }
+}
---
+++ Backend/DatabaseContext.cs
@@ -0,0 +1,45 @@
+﻿using Backend.Models;
+using MySql.Data.MySqlClient;
+
+namespace Backend
+{
+    public class DatabaseContext
+    {
+        public string ConnectionString { get; set; }
+
+        public DatabaseContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            var list = new List<UserModel>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM users", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new UserModel()
+                        {
+                            UserId = reader.GetInt32("user_id"),
+                            UserName = reader.GetString("username"),
+                            Password = reader.GetString("password"),
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
