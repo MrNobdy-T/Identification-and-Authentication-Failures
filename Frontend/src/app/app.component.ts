3761a884a6292039fc4d6d74ac77ce4bcc245abf
Benjamin BOGNER <BOGNER.Benjamin@outlook.at>
2023-05-12 14:42:30 +0200

    Added basic state for logged in users.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,11 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-login></app-login>
+    <app-login *ngIf="isLoggedIn === false" (OnSuccessfulLogin)="OnSuccessfulLogin()"></app-login>
+    
+    <div id="messageContainer" *ngIf="isLoggedIn">
+      <h1 id="loginMessage">Successfully identified and authenticated.</h1>
+    </div>
+    
     <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'Identification and Authentication Failures';
+  title:string = 'Identification and Authentication Failures';
+  isLoggedIn: boolean = false;
+
+  OnSuccessfulLogin() {
+    this.isLoggedIn = true;
+    }
+
+  OnLogout() {
+    this.isLoggedIn = false;
+  }
 }
